b44d83c289c0928bb840d78b9a9819c7b0d570cc
Alex Navasardyan <twokul@users.noreply.github.com>
2015-03-31 15:20:25 -0700

    Bumps the broccoli-jscs version up

--- blueprints/frontend-shared-config/index.js
+++ blueprints/frontend-shared-config/index.js
@@ -4,7 +4,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    this.addPackageToProject('broccoli-jscs', '0.0.21');
+    this.addPackageToProject('broccoli-jscs', '0.0.22');
     this.addPackageToProject('broccoli-sass', '^0.6.2');
     this.addPackageToProject('git-repo-info', '^1.0.4');
     this.addPackageToProject('ember-cli-yuidoc', '0.6.4');
