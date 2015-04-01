30664d1adf33cfb20215b65dafaa8770e2585463
Danny Alvarez <danny@ticketfly.com>
2015-04-01 13:09:47 -0700

    Explicitly setting broccoli-sass to 0.6.2 until they fix the bug in 0.6.3

--- blueprints/frontend-shared-config/index.js
+++ blueprints/frontend-shared-config/index.js
@@ -5,7 +5,7 @@ module.exports = {
 
   afterInstall: function() {
     this.addPackageToProject('broccoli-jscs', '0.0.22');
-    this.addPackageToProject('broccoli-sass', '^0.6.2');
+    this.addPackageToProject('broccoli-sass', '0.6.2');
     this.addPackageToProject('git-repo-info', '^1.0.4');
     this.addPackageToProject('ember-cli-yuidoc', '0.6.4');
     this.addPackageToProject('ember-cli-bugsnag', '^0.0.7');
